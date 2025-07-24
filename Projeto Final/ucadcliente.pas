6d4015c69578d09e928c8ac6f59c2aa443770779
Danilo-Toriy <randomlittlest@gmail.com>
2025-07-24 17:47:57 -0300

    Add files via upload

--- Projeto Final/ucadcliente.pas
+++ Projeto Final/ucadcliente.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, DBCtrls,
-  StdCtrls, uxcadmodelo, DB;
+  StdCtrls, uxcadmodelo, DB, dm;
 
 type
 
@@ -14,6 +14,8 @@ type
 
   TCadClienteF = class(TXCadModelo)
     dbComboBType: TDBComboBox;
+    dbedTel: TDBEdit;
+    dbedtEmail: TDBEdit;
     dbedtClient: TDBEdit;
     dbedtCPF: TDBEdit;
     dbedtClientID: TDBEdit;
@@ -21,12 +23,24 @@ type
     Label1: TLabel;
     Label2: TLabel;
     Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
     LabelCPF: TLabel;
     Panel3: TPanel;
     panelDiv: TPanel;
+    procedure btnCancelClick(Sender: TObject);
+    procedure btnDelClick(Sender: TObject);
+    procedure btnEditClick(Sender: TObject);
+    procedure btnNewClick(Sender: TObject);
+    procedure btnRecClick(Sender: TObject);
     procedure dbComboBTypeChange(Sender: TObject);
+    procedure dbedTelChange(Sender: TObject);
+    procedure DBGridModelDblClick(Sender: TObject);
+    procedure dsClientesStateChange(Sender: TObject);
     procedure edtSearchChange(Sender: TObject);
-    procedure Label3Click(Sender: TObject);
+    procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
+    procedure FormCreate(Sender: TObject);
+    procedure tbCadastroHide(Sender: TObject);
   private
 
   public
@@ -44,27 +58,186 @@ implementation
 
 procedure TCadClienteF.edtSearchChange(Sender: TObject);
 begin
-  inherited;
+  DataModule1.zqryClientes.Close;
+  DataModule1.zqryClientes.SQL.Clear;
+  DataModule1.zqryClientes.SQL.Add('SELECT '+
+                                           '* '+
+                                           'FROM '+
+                                           'cliente '+
+                                           'WHERE '+
+                                           'CAST(clienteid AS TEXT) ILIKE :termo '+
+                                           'OR cpf_cnpj_cliente ILIKE :termo '+
+                                           'OR nome_cliente ILIKE :termo ' +
+                                           'OR telefone_cliente ILIKE :termo '+
+                                           'OR email_cliente ILIKE :termo');
+  DataModule1.zqryClientes.ParamByName('termo').AsString := '%' + edtSearch.Text + '%';
+  DataModule1.zqryClientes.Open;
+end;
+
+procedure TCadClienteF.FormClose(Sender: TObject; var CloseAction: TCloseAction
+  );
+begin
+  DataModule1.zqryClientes.Close;
+  CloseAction := caFree;
+end;
+
+procedure TCadClienteF.FormCreate(Sender: TObject);
+begin
+  DataModule1.zqryClientes.Open;
+end;
+
+procedure TCadClienteF.tbCadastroHide(Sender: TObject);
+begin
+  if dsClientes.State in [dsEdit, dsInsert] then
+     begin
+        if ( MessageDlg ('As alterações sendo feitas poderão ser perdidas. Deseja realmente trocar de aba?', mtConfirmation, [mbYes, mbNo], 0) = mrYes) then
+     begin
+        DataModule1.zqryClientes.Cancel;
+        PageControl1.ActivePage := tbPesquisa;
+        dbedtClient.Enabled := false;
+        dbComboBType.Enabled := false;
+        dbedTel.Enabled := false;
+        dbedtEmail.Enabled := false;
+     end
+     else
+         begin
+              PageControl1.ActivePage := tbCadastro;
+              dbedtClient.Enabled := true;
+              dbComboBType.Enabled := true;
+              dbedTel.Enabled := true;
+              dbedtEmail.Enabled := true;
+         end;
+     end;
 end;
 
 procedure TCadClienteF.dbComboBTypeChange(Sender: TObject);
 begin
     if (dbComboBType.ItemIndex = 0) OR (dbComboBType.ItemIndex = 3) then
        begin
-          LabelCPF.Caption := 'CNPJ:';
+          LabelCPF.Caption := 'CPF:';
           dbedtCPF.Enabled := true;
        end
        else
            begin
-             LabelCPF.Caption := 'CPF:';
+             LabelCPF.Caption := 'CNPJ:';
              dbedtCPF.Enabled := true;
            end;
 end;
 
-procedure TCadClienteF.Label3Click(Sender: TObject);
+procedure TCadClienteF.dbedTelChange(Sender: TObject);
 begin
 
 end;
+
+procedure TCadClienteF.DBGridModelDblClick(Sender: TObject);
+begin
+  PageControl1.ActivePage := tbCadastro;
+end;
+
+procedure TCadClienteF.btnNewClick(Sender: TObject);
+begin
+  PageControl1.ActivePage := tbCadastro;
+  DataModule1.zqryClientes.Insert;
+  dbedtClient.Enabled := true;
+  dbComboBType.Enabled := true;
+  dbedTel.Enabled := true;
+  dbedtEmail.Enabled := true;
+
+  DataModule1.qryGenerica.Close;
+  DataModule1.qryGenerica.UpdateObject := DataModule1.updtClientes;
+  DataModule1.qryGenerica.SQL.Clear;
+  DataModule1.qryGenerica.SQL.Add('select nextval( ' +QuotedStr('cliente_idtemp') +') as aux');
+  DataModule1.qryGenerica.Open;
+
+  DataModule1.zqryClientesclienteid.AsInteger := DataModule1.qryGenerica.FieldByName('aux').AsInteger;
+end;
+
+procedure TCadClienteF.btnRecClick(Sender: TObject);
+begin
+  if DataModule1.zqryClientesnome_cliente.AsString = '' then
+     begin
+        Showmessage('Por favor, informe pelo menos um nome do cliente!');
+        dbedtClient.SetFocus;
+     end
+     else
+         begin
+           if dbComboBType.ItemIndex = -1 then
+              begin
+                 Showmessage('Escolha uma opção válida!');
+                 dbComboBType.SetFocus;
+              end
+              else
+                  begin
+                    if DataModule1.zqryClientescpf_cnpj_cliente.AsString = '' then
+                       begin
+                          Showmessage('Informe um CPF ou CNPJ!');
+                          dbedtCPF.SetFocus;
+                       end
+                       else
+                           begin
+                             if (DataModule1.zqryClientesemail_cliente.AsString = '') AND (DataModule1.zqryClientestelefone_cliente.AsString = '') then
+                                begin
+                                   ShowMessage('Informe pelo menos um meio de contato com o cliente!');
+                                   dbedTel.SetFocus;
+                                end
+                                else
+                                    begin
+                                         if Trim(DataModule1.zqryClientesemail_cliente.AsString) = '' then
+                                            DataModule1.zqryClientesemail_cliente.AsString := 'Email não informado';
+                                         if Trim(DataModule1.zqryClientestelefone_cliente.AsString) = '' then
+                                            DataModule1.zqryClientestelefone_cliente.AsString := 'Não cadastrado';
+
+                                         DataModule1.zqryClientes.Post;
+                                         PageControl1.ActivePage := tbPesquisa;
+                                    end;
+                           end;
+                  end;
+         end;
+end;
+
+procedure TCadClienteF.btnCancelClick(Sender: TObject);
+begin
+  DataModule1.zqryClientes.Cancel;
+  PageControl1.ActivePage := tbPesquisa;
+  dbedtClient.Enabled := false;
+  dbComboBType.Enabled := false;
+  dbedTel.Enabled := false;
+  dbedtEmail.Enabled := false;
+
+end;
+
+procedure TCadClienteF.btnDelClick(Sender: TObject);
+begin
+  if ( MessageDlg ('Deseja realmente excluir esse cliente?', mtConfirmation, [mbYes, mbNo], 0) = mrYes) then
+     begin
+        DataModule1.zqryClientes.Delete;
+        PageControl1.ActivePage := tbPesquisa;
+        dbedtClient.Enabled := false;
+        dbComboBType.Enabled := false;
+        dbedTel.Enabled := false;
+        dbedtEmail.Enabled := false
+     end;
+end;
+
+procedure TCadClienteF.btnEditClick(Sender: TObject);
+begin
+  DataModule1.zqryClientes.Edit;
+  dbedtClient.Enabled := true;
+  dbComboBType.Enabled := true;
+  dbedTel.Enabled := true;
+  dbedtEmail.Enabled := true;
+end;
+
+procedure TCadClienteF.dsClientesStateChange(Sender: TObject);
+begin
+   btnNew.Enabled := (Sender as TDataSource).State in [dsBrowse];
+   btnRec.Enabled := (Sender as TDataSource).State in [dsEdit, dsInsert];
+   btnCancel.Enabled := btnRec.Enabled;
+   btnEdit.Enabled := (btnNew.Enabled) and not ((Sender as TDataSource).DataSet.IsEmpty);
+   btnDel.Enabled := btnEdit.Enabled;
+   btnClose.Enabled := btnNew.Enabled;
+end;
+
 
 end.
 
---
+++ Projeto Final/ucadprodutos.pas
@@ -0,0 +1,270 @@
+unit uCadProdutos;
+
+{$mode ObjFPC}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, DBCtrls,
+  StdCtrls, uxcadmodelo, DB, dm;
+
+type
+
+  { TCadProdutosF }
+
+  TCadProdutosF = class(TXCadModelo)
+    dbComboBCat: TDBComboBox;
+    dbradio: TDBRadioGroup;
+    dsProdutos: TDataSource;
+    dbedtProd: TDBEdit;
+    dbedtProdID: TDBEdit;
+    dbedtObs: TDBEdit;
+    dbedtVal: TDBEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    Panel3: TPanel;
+    panelDiv: TPanel;
+    procedure btnCancelClick(Sender: TObject);
+    procedure btnDelClick(Sender: TObject);
+    procedure btnEditClick(Sender: TObject);
+    procedure btnNewClick(Sender: TObject);
+    procedure btnRecClick(Sender: TObject);
+    procedure dbComboBCatClick(Sender: TObject);
+    procedure DBGridModelDblClick(Sender: TObject);
+    procedure dsProdutosDataChange(Sender: TObject; Field: TField);
+    procedure dsProdutosStateChange(Sender: TObject);
+    procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
+    procedure FormCreate(Sender: TObject);
+    procedure Label3Click(Sender: TObject);
+    procedure Label4Click(Sender: TObject);
+    procedure Label5Click(Sender: TObject);
+    procedure tbCadastroHide(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  CadProdutosF: TCadProdutosF;
+
+implementation
+
+{$R *.lfm}
+
+{ TCadProdutosF }
+
+procedure TCadProdutosF.dsProdutosDataChange(Sender: TObject; Field: TField);
+begin
+
+end;
+
+procedure TCadProdutosF.dsProdutosStateChange(Sender: TObject);
+begin
+   btnNew.Enabled := (Sender as TDataSource).State in [dsBrowse];
+   btnRec.Enabled := (Sender as TDataSource).State in [dsEdit, dsInsert];
+   btnCancel.Enabled := btnRec.Enabled;
+   btnEdit.Enabled := (btnNew.Enabled) and not ((Sender as TDataSource).DataSet.IsEmpty);
+   btnDel.Enabled := btnEdit.Enabled;
+   btnClose.Enabled := btnNew.Enabled;
+end;
+
+procedure TCadProdutosF.FormClose(Sender: TObject; var CloseAction: TCloseAction
+  );
+begin
+  DataModule1.zqryProdutos.Close;
+  DataModule1.zqryCategorias.Close;
+  CloseAction := caFree;
+end;
+
+procedure TCadProdutosF.btnNewClick(Sender: TObject);
+begin
+  PageControl1.ActivePage := tbCadastro;
+  DataModule1.zqryProdutos.Insert;
+  dbedtProd.Enabled := true;
+  dbComboBCat.Enabled := true;
+  dbedtObs.Enabled := true;
+  dbedtVal.Enabled := true;
+  dbradio.Enabled := true;
+
+  DataModule1.qryGenerica.Close;
+  DataModule1.qryGenerica.UpdateObject := DataModule1.updtProdutos;
+  DataModule1.qryGenerica.SQL.Clear;
+  DataModule1.qryGenerica.SQL.Add('select nextval( ' +QuotedStr('produto_idtemp') +') as helper');
+  DataModule1.qryGenerica.Open;
+
+  DataModule1.zqryProdutosprodutoid.AsInteger := DataModule1.qryGenerica.FieldByName('helper').AsInteger;
+end;
+
+procedure TCadProdutosF.btnRecClick(Sender: TObject);
+var
+  valor: Double;
+  id: integer;
+begin
+  if DataModule1.zqryProdutosds_produto.AsString = '' then
+     begin
+          ShowMessage('Insira o nome do produto!!');
+          dbedtProd.SetFocus;
+     end
+     else
+         begin
+              if dbComboBCat.ItemIndex = -1 then
+                 begin
+                      Showmessage('Insira uma categoria!!');
+                      dbComboBCat.SetFocus;
+                 end
+                 else
+                     begin
+                          if DataModule1.zqryProdutosvl_venda_produto.AsString = '' then
+                             begin
+                             ShowMessage('Informe um valor!!');
+                             dbedtVal.SetFocus;
+                             end
+                             else
+                                 begin
+                                      if dbradio.ItemIndex = -1 then
+                                         begin
+                                         ShowMessage('Informe o status!!');
+                                         dbradio.SetFocus;
+                                         end
+                                         else
+                                             begin
+                                                  if TryStrToFloat(StringReplace(DataModule1.zqryProdutosvl_venda_produto.AsString, ',', '.', [rfReplaceAll]), valor) then
+                                                     begin
+                                                         if DataModule1.zqryProdutosobs_produto.AsString = '' then
+                                                            DataModule1.zqryProdutosobs_produto.AsString := 'Sem descrição';
+                                                         DataModule1.zqryProdutosvl_venda_produto.AsFloat := valor;
+                                                         DataModule1.zqryProdutosdt_cadastro_produto.AsDateTime := Date;
+                                                         id := Integer(dbComboBCat.items.Objects[dbComboBCat.ItemIndex]);
+                                                         DataModule1.zqryProdutoscategoriaprodutoid.AsInteger := id;
+                                                         DataModule1.zqryProdutos.Post;
+                                                         PageControl1.ActivePage := tbPesquisa;
+                                                     end
+                                                     else
+                                                         begin
+                                                              Showmessage('Informe um valor válido!!');
+                                                              dbedtVal.SetFocus;
+                                                         end;
+
+                                             end;
+                                 end;
+                     end;
+
+         end;
+end;
+
+procedure TCadProdutosF.dbComboBCatClick(Sender: TObject);
+begin
+
+end;
+
+procedure TCadProdutosF.DBGridModelDblClick(Sender: TObject);
+begin
+  PageControl1.ActivePage := tbCadastro;
+end;
+
+procedure TCadProdutosF.btnEditClick(Sender: TObject);
+begin
+  DataModule1.zqryProdutos.Edit;
+  dbedtProd.Enabled := true;
+  dbComboBCat.Enabled := true;
+  dbedtObs.Enabled := true;
+  dbedtVal.Enabled := true;
+  dbradio.Enabled := true;
+end;
+
+procedure TCadProdutosF.btnDelClick(Sender: TObject);
+begin
+  if ( MessageDlg ('Deseja realmente excluir esse produto?', mtConfirmation, [mbYes, mbNo], 0) = mrYes) then
+     begin
+        DataModule1.zqryProdutos.Delete;
+        PageControl1.ActivePage := tbPesquisa;
+        dbedtProd.Enabled := false;
+        dbComboBCat.Enabled := false;
+        dbedtObs.Enabled := false;
+        dbedtVal.Enabled := false;
+        dbradio.Enabled := false;
+     end;
+end;
+
+procedure TCadProdutosF.btnCancelClick(Sender: TObject);
+begin
+  DataModule1.zqryProdutos.Cancel;
+  PageControl1.ActivePage := tbPesquisa;
+  dbedtProd.Enabled := false;
+  dbComboBCat.Enabled := false;
+  dbedtObs.Enabled := false;
+  dbedtVal.Enabled := false;
+  dbradio.Enabled := false;
+end;
+
+procedure TCadProdutosF.FormCreate(Sender: TObject);
+begin
+  DataModule1.zqryProdutos.Open;
+  dbComboBCat.Items.Clear;
+
+  DataModule1.zqryCategorias.Close;
+  DataModule1.zqryCategorias.SQL.Clear;
+  DataModule1.zqryCategorias.SQL.Add('SELECT '+
+                                 '* '+
+                                 'FROM categoria_produto '+
+                                 'ORDER BY '+
+                                 'ds_categoria_produto');
+  DataModule1.zqryCategorias.Open;
+
+  DataModule1.zqryCategorias.First;
+  while not DataModule1.zqryCategorias.EOF do
+        begin
+          dbComboBCat.Items.AddObject(DataModule1.zqryCategorias.FieldByName('ds_categoria_produto').AsString,
+          TObject(DataModule1.zqryCategorias.FieldByName('categoriaprodutoid').AsInteger));
+          DataModule1.zqryCategorias.Next;
+        end;
+end;
+
+procedure TCadProdutosF.Label3Click(Sender: TObject);
+begin
+
+end;
+
+procedure TCadProdutosF.Label4Click(Sender: TObject);
+begin
+
+end;
+
+procedure TCadProdutosF.Label5Click(Sender: TObject);
+begin
+
+end;
+
+procedure TCadProdutosF.tbCadastroHide(Sender: TObject);
+begin
+  if dsProdutos.State in [dsEdit, dsInsert] then
+     begin
+        if ( MessageDlg ('As alterações sendo feitas poderão ser perdidas. Deseja realmente trocar de aba?', mtConfirmation, [mbYes, mbNo], 0) = mrYes) then
+     begin
+        DataModule1.zqryProdutos.Cancel;
+        PageControl1.ActivePage := tbPesquisa;
+        dbedtProd.Enabled := false;
+        dbComboBCat.Enabled := false;
+        dbedtObs.Enabled := false;
+        dbedtVal.Enabled := false;
+        dbradio.Enabled := false;
+     end
+     else
+         begin
+              PageControl1.ActivePage := tbCadastro;
+              dbedtProd.Enabled := true;
+              dbComboBCat.Enabled := true;
+              dbedtObs.Enabled := true;
+              dbedtVal.Enabled := true;
+              dbradio.Enabled := true;
+         end;
+     end;
+end;
+
+end.
+
